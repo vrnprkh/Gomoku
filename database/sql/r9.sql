4dacdf6f2b49e4318ad136ae1f9aa4d39131ee6a
Lexie Zhang <zay20040811@gmail.com>
2024-06-20 13:32:57 -0400

    updated the query for testing for r9

--- database/sql/r9.sql
+++ database/sql/r9.sql
@@ -4,7 +4,7 @@
 --such as adding friends and request to play with certain users. 
 
 
-
+--SAMPLE QUERY: 
 --if checkbox "search for friends only" is selected, the output of the query below will be displayed
 --player with uid @player_id can search for their friends with username similar to @friend_name
 SELECT
@@ -27,4 +27,40 @@ WHERE
 --player with uid @player_id can search for other users with username similar to @user_name
 SELECT uid
 FROM Users
+WHERE username REGEXP @user_name;
+
+
+
+
+-------------------------------TESTABLE QUERY: Run this query in the actual database----------------------
+--if checkbox "search for friends only" is selected, the output of the query below will be displayed
+--player with uid @player_id can search for their friends with username similar to @friend_name
+SET @player_id = 1;
+SET @friend_name = 'name6';
+SELECT
+    CASE
+        WHEN f.uid1 = @player_id THEN f.uid2
+        WHEN f.uid2 = @player_id THEN f.uid1
+    END AS friend_uid, 
+    CASE
+        WHEN f.uid1 = @player_id THEN u2.username
+        WHEN f.uid2 = @player_id THEN u1.username
+    END AS friend_username
+    
+FROM
+    test_database.Friends f
+JOIN
+    test_database.Users u1 ON f.uid1 = u1.uid
+JOIN 
+    test_database.Users u2 ON f.uid2 = u2.uid
+WHERE 
+    (f.uid1 = @player_id AND u2.username REGEXP @friend_name)
+    OR (f.uid2 = @player_id AND u1.username REGEXP @friend_name);
+
+--if checkbox "search for friends only" is not selected, the output of the query below will be displayed
+--player with uid @player_id can search for other users with username similar to @user_name
+SET @user_name = 'name4';
+
+SELECT uid, username
+FROM test_database.Users
 WHERE username REGEXP @user_name;
