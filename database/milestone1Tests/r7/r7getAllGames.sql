7948a45fbaee00e29be9a47cd1737cc6a7981532
vrnprkh <paintingburritos@gmail.com>
2024-06-20 21:18:35 -0400

    finalized feature r7

---
+++ database/milestone1Tests/r7/r7getAllGames.sql
@@ -0,0 +1,17 @@
+-- get all games
+SET @player1 = 5;
+
+SELECT 
+    G.final_game_state,
+    U1.username AS player1,
+    U2.username AS player2,
+    G.result
+FROM 
+    Games G
+JOIN 
+    Users U1 ON G.uid1 = U1.uid
+JOIN 
+    Users U2 ON G.uid2 = U2.uid
+WHERE 
+    (G.uid1 = @player1 OR G.uid2 = @player1)
+    AND G.gid NOT IN (SELECT gid FROM Lobbies);
--- database/milestone1Tests/r7/r7getFavourites.sql
+++ database/milestone1Tests/r7/r7getFavourites.sql
@@ -1,25 +1,8 @@
--- get all games
-SET @player1 = 5;
-
-SELECT 
-    G.final_game_state,
-    U1.username AS player1,
-    U2.username AS player2,
-    G.result
-FROM 
-    Games G
-JOIN 
-    Users U1 ON G.uid1 = U1.uid
-JOIN 
-    Users U2 ON G.uid2 = U2.uid
-WHERE 
-    (G.uid1 = @player1 OR G.uid2 = @player1)
-    AND G.gid NOT IN (SELECT gid FROM Lobbies);
 
 -- Set certain games to favourites
-SET @favGID1 = 0;
-SET @favGID2 = 1;
-SET @favGID3 = 2;
+SET @favGID1 = 1009;
+SET @favGID2 = 1024;
+SET @favGID3 = 1077;
 
 INSERT INTO FavouriteGames (uid, gid)
 VALUES (@player1, @favGID1);
@@ -56,22 +39,3 @@ DELETE FROM FavouriteGames
 WHERE uid = @player1 AND gid = @favGID2;
 DELETE FROM FavouriteGames
 WHERE uid = @player1 AND gid = @favGID3;
-
--- get favourites of same user (empty)
-SELECT 
-    G.final_game_state,
-    U1.username AS player1,
-    U2.username AS player2,
-    G.result
-FROM 
-    Games G
-JOIN 
-    Users U1 ON G.uid1 = U1.uid
-JOIN 
-    Users U2 ON G.uid2 = U2.uid
-JOIN 
-    FavouriteGames FG ON G.gid = FG.gid
-WHERE 
-    FG.uid = @player1
-    AND (G.uid1 = @player1 OR G.uid2 = @player1)
-    AND G.gid NOT IN (SELECT gid FROM Lobbies);
