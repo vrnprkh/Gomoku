4dc6e53b443971e3c2db3097dc8289d5717e445c
vrnprkh <paintingburritos@gmail.com>
2024-07-22 23:01:02 -0400

    basic polling

---
+++ backend/app/inGame.py
@@ -0,0 +1,87 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+import mysql.connector
+import logging
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+
+
+
+load_dotenv()
+
+
+db_user = os.getenv("DB_USER")
+db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+
+
+in_game_bp = Blueprint('ingame', __name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+def get_db_connection():
+    try:
+        conn = mysql.connector.connect(
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=3306,
+            database=db_name,
+            ssl_ca="DigiCertGlobalRootG2.crt.pem",
+            ssl_disabled=False
+        )
+        logger.info("Database connection successful")
+        return conn
+    except Exception as e:
+        logger.error(f"Database connection failed: {str(e)}", exc_info=True)
+        raise
+
+
+
+
+@in_game_bp.route('/poll-turn', methods=['GET'])
+@jwt_required()
+def poll_turn():
+    try:
+        current_user_id = get_jwt_identity()
+        gid = request.args.get('gid')
+
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        queryGame = """
+        Select *
+        FROM Games g
+        WHERE g.uid1 = %s or g.uid2 = %s
+        """
+
+        cursor.execute(queryGame, [current_user_id, current_user_id])
+        game = cursor.fetchone()
+        if game == None:
+            # user is not in this game
+            cursor.close()
+            conn.close()
+            return jsonify(), 500
+        
+        
+        isPlayerOne = current_user_id == game["uid1"]
+
+        queryMoves = """
+        SELECT move_number
+        FROM DetailedMoves dm
+        WHERE gid = %s
+        """
+
+
+        cursor.execute(queryMoves, [gid])
+        moves = cursor.fetchall()
+        cursor.close()
+        conn.close()
+
+
+        return jsonify({"turn" : bool((len(moves) % 2) ==  (not isPlayerOne))})
+    except Exception as e:
+        logger.error(f"Error fetching turn: {str(e)}", exc_info=True)
+        return jsonify({'message': 'Failed to fetch friend turn'}), 500
